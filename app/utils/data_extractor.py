d6b1c1548a0abb3402eaa75c33b735b823e3efb4
Pinni Plevinski <shareytora007@gmail.com>
2025-09-07 12:02:26 +0300

    Extracting all file paths in the directory - initial

--- app/utils/data_extractor.py
+++ app/utils/data_extractor.py
@@ -1,3 +1,13 @@
+from pathlib import Path
+
 class DataExtractor:
     """A utility class to extract metadata from binary files"""
-    pass
+    def __init__(self, dir_path):
+        self._dir_path = Path(dir_path)
+
+    def get_all_in_dir(self):
+        return self._dir_path.glob("*")
+
+
+if __name__ == "__main__":
+    print(list(DataExtractor('C:\\podcasts').get_all_in_dir()))
