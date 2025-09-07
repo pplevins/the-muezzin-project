fd8c4b7e5180f2da5769e06b55e5d5bde7f95a20
Pinni Plevinski <shareytora007@gmail.com>
2025-09-07 16:14:47 +0300

    Setting elasticsearch and kibana

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,3 +1,6 @@
 @REM Setting up the kafka container - for local running
 @REM (for containerization need to set a network and environment variables)
 docker run -d -p 9092:9092 --name broker apache/kafka:latest
+
+@REM Setting the elastic-search and kibana stack in the compose.yaml
+docker compose -f compose.yaml up -d
