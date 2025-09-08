d0a9e10e7bcf9ead9b06206f71a50025e8f77f45
Pinni Plevinski <shareytora007@gmail.com>
2025-09-08 12:31:13 +0300

    Adding error handling and logging procedure

--- app/services/processor/data_processor.py
+++ app/services/processor/data_processor.py
@@ -1,6 +1,10 @@
+from elasticsearch import ApiError
+from kafka.errors import NoBrokersAvailable, KafkaError
+from pymongo.errors import PyMongoError
+
 from app.core import Database
 from app.dal import PodcastsDal
-from app.models import Consumer, ElasticSearchClient
+from app.models import Consumer, ElasticSearchClient, Logger
 from app.utils import DataHash
 
 
@@ -9,6 +13,7 @@ class DataProcessor:
         self._consumer = Consumer(kafka_topic, kafka_broker)
         self._es_client = ElasticSearchClient(kafka_topic)
         self._dal = PodcastsDal(Database())
+        self._logger = Logger.get_logger()
 
     def _add_unique_id(self, msg):
         msg_str = f'{msg["name"] + msg["created_at"] + str(msg["size_in_bytes"]) + msg["file_type"]}'
@@ -19,8 +24,25 @@ class DataProcessor:
         await self._dal.insert_document("podcasts", msg)
 
     async def process(self):
-        for msg in self._consumer.get_consumed_messages():
-            msg_dict = self._add_unique_id(msg.value)
-            print(msg_dict)
-            self._es_client.load_to_es(msg_dict)
-            await self._insert_file_to_db(msg_dict)
+        try:
+            for msg in self._consumer.get_consumed_messages():
+                try:
+                    msg_dict = self._add_unique_id(msg.value)
+                    self._es_client.load_to_es(msg_dict)
+                    self._logger.info(f"Inserted message id {msg_dict["unique_id"]} to Elastic.")
+                    await self._insert_file_to_db(msg_dict)
+                    self._logger.info(f"Inserted file id {msg_dict["unique_id"]} to MongoDB.")
+
+                except ApiError as e:
+                    self._logger.error(f"Failed to insert message {msg.value} to Elastic: {e}")
+                except PyMongoError as e:
+                    self._logger.error(f"Failed to insert message {msg.value} to MongoDB: {e}")
+                except Exception as e:
+                    self._logger.error(f"An unexpected error occurred while processing massage {msg.value}: {e}")
+
+        except NoBrokersAvailable as e:
+            self._logger.error(f"Kafka connection error: {e}")
+        except KafkaError as e:
+            self._logger.error(f"Kafka error during consumption: {e}")
+        except Exception as e:
+            self._logger.error(f"Unexpected error: {e}")
--- app/services/publisher/data_publisher.py
+++ app/services/publisher/data_publisher.py
@@ -1,4 +1,6 @@
-from app.models import Producer
+from kafka.errors import NoBrokersAvailable, KafkaError
+
+from app.models import Producer, Logger
 from app.utils import MetadataExtractor
 
 
@@ -9,9 +11,19 @@ class DataPublisher:
         """Constructor."""
         self._producer = Producer(kafka_url)
         self._extractor = MetadataExtractor(files_dir)
+        self._logger = Logger.get_logger()
 
     def extract_and_publish(self, topic):
         """Extract the metadata from all the files, and publish it to a given topic in Kafka."""
         for file in self._extractor.get_all_in_dir():
-            metadata = self._extractor.extract_metadata(file)
-            self._producer.publish_massage(topic, metadata)
+            try:
+                metadata = self._extractor.extract_metadata(file)
+                self._producer.publish_massage(topic, metadata)
+                self._logger.info(f"Published file {metadata["name"]} to Kafka.")
+
+            except NoBrokersAvailable as e:
+                self._logger.error(f"Kafka connection error: {e}")
+            except KafkaError as e:
+                self._logger.error(f"Kafka error during consumption: {e}")
+            except Exception as e:
+                self._logger.error(f"Unexpected error: {e}")
