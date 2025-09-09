85313ac0d4aa3013118bb04933d7252a4408a03e
Pinni Plevinski <shareytora007@gmail.com>
2025-09-09 18:01:08 +0300

    basic term frequency

--- app/services/classifier/data_classifier.py
+++ app/services/classifier/data_classifier.py
@@ -12,3 +12,16 @@ class DataClassifier:
                                    .decrypt_str_base64(
             "RnJlZWRvbSBGbG90aWxsYSxSZXNpc3RhbmNlLExpYmVyYXRpb24sRnJlZSBQYWxlc3RpbmUsR2F6YSxDZWFzZWZpcmUsUHJvdGVzdCxVTlJXQQ==")
                                    .lower().split(","))
+
+    def _calculate_term_frequency(self, text):
+        danger_score = 0
+        words_in_text = text.split(" ")
+        for word in self._non_hostile_words:
+            danger_score += (text.lower().count(word) / len(words_in_text))
+        for word in self._hostile_words:
+            danger_score += (text.lower().count(word) / len(words_in_text)) * 2
+        return danger_score
+
+
+print(DataClassifier()._calculate_term_frequency(
+    " The displacement today is not new, it echoes the Nakba, families torn from their homes generation after generation. Exactly. Refugees in camps across the region still carry the keys to houses they can't return to. And each new massacre adds to that history of exile. That's why protests matter. They link past and present, showing the occupation isn't temporary, it's systemic, and movements like BDS remind the world that apartheid has no place in the modern era. Liberation means return, justice, and an end to endless displacement."))
