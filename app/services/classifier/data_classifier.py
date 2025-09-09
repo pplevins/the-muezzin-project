d78e0d7af9b36e2f91ca9552328476244d729c31
Pinni Plevinski <shareytora007@gmail.com>
2025-09-09 17:17:56 +0300

    Adding hostile words list to classifier

--- app/services/classifier/data_classifier.py
+++ app/services/classifier/data_classifier.py
@@ -1,2 +1,14 @@
+from app.utils import WordsDecryptor
+
+
 class DataClassifier:
-    pass
+    def __init__(self):
+        self._hostile_words = (WordsDecryptor
+                               .decrypt_str_base64(
+            "R2Vub2NpZGUsV2FyIENyaW1lcyxBcGFydGhlaWQsTWFzc2FjcmUsTmFrYmEsRGlzcGxhY2VtZW50LEh1bWFuaXRhcmlhbiBDcmlzaXMsQmxvY2thZGUsT2NjdXBhdGlvbixSZWZ1Z2VlcyxJQ0MsQkRT")
+                               .lower()
+                               .split(","))
+        self._non_hostile_words = (WordsDecryptor
+                                   .decrypt_str_base64(
+            "RnJlZWRvbSBGbG90aWxsYSxSZXNpc3RhbmNlLExpYmVyYXRpb24sRnJlZSBQYWxlc3RpbmUsR2F6YSxDZWFzZWZpcmUsUHJvdGVzdCxVTlJXQQ==")
+                                   .lower().split(","))
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,3 +2,4 @@ from .audio_transcriber import AudioTranscriber
 from .binary_file_handler import BinaryFileHandler
 from .data_hash import DataHash
 from .metadata_extractor import MetadataExtractor
+from .words_decryptor import WordsDecryptor
